6a5a75ef582651cef4d10b69aa1c73f2fc93f0df
anthonyero <anthonyero@gmail.com>
2024-05-07 20:28:20 -0500

    Imported and created a router to handle user requests to endpoints and specific components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+import App from './App';
+
+/* Import pages once there components are written
+
+- About Me
+- Portfolio
+- Contact
+- Resume
+
+*/
+
+// Create the router as well as the accessible routes and their respective component
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      },
+      {
+        path: '/About',
+        element: <About />
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/Contact',
+        element: <Contact />
+      },
+      {
+        path: 'Resume',
+        element: <Resume />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
