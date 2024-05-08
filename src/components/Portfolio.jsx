7876a8ffc3199fc2490d77c4a25065ee6f2805ef
anthonyero <anthonyero@gmail.com>
2024-05-07 22:46:36 -0500

    Adjusted md grid item space for medium sized screens, allowing for wider project elements

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ export default function ResponsiveGrid() {
     <>
       <Grid container justifyContent='space-evenly' alignItems='stretch'>
         {Array.from(Array(6)).map((_, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
+          <Grid item xs={12} sm={6} md={6} key={index}>
             <p>Project: {index + 1}</p>
           </Grid>
         ))}
