ae1457678e8530335bee40ccdb9780a0a9b2bb68
anthonyero <anthonyero@gmail.com>
2024-05-07 20:47:05 -0500

    Deleted empty space and added a semicolon to one line. Stylistic change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-
 import App from './App';
 
 /* Import pages once there components are written
@@ -45,4 +44,4 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
