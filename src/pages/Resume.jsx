e6555db307646d06a69c43617c7ff350f779fcff
anthonyero <anthonyero@gmail.com>
2024-05-09 01:35:30 -0500

    Added a download link for my resume. Updated my lists of proficiencies

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,14 +2,27 @@ export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
+      <a href='../src/assets/Rodriguez-Ortiz Anthony - Resume (April 2024).pdf' download>Download a copy of my resume! </a>
       <h2>Front-end Proficiencies</h2>
         <ul>
-          <li></li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Responsive design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
         </ul>
 
       <h2>Back-end Proficiencies</h2>
       <ul>
-        <li></li>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>PostgreSQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
       </ul> 
     </div>
   );
