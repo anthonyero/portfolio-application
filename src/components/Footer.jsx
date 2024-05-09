554c9f55266c3c7664a6914c689b4c89eeaf1109
anthonyero <anthonyero@gmail.com>
2024-05-09 16:55:00 -0500

    Added icons to my anchor links along with styling. Also, added a link to my PubMed search results

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,32 @@
 export default function Footer() {
+  const styles = {
+    footer: {
+      display: 'flex',
+      justifyContener: 'center',
+      alignItems: 'center'
+    },
+    github: {
+      width: '50%',
+      height: '50%',
+      margin: '20px'
+    },
+    linkedin: {
+      width: '60%',
+      height: '60%',
+      margin: '20px'
+    },
+    pubmed: {
+      width: '60%',
+      height: '60%',
+      margin: '20px'
+    }
+  };
+
   return (
-    <div>
-      <ul>
-        <li> 
-          <a href='https://www.github.com/anthonyero'>GitHub</a>
-        </li>
-        <li>
-          <a href='https://www.linkedin.com/in/anthony-rodriguez-ortiz'>LinkedIn</a>
-        </li>
-      
-      </ul>
+    <div className='footer' style={styles.footer}>
+          <a href='https://www.github.com/anthonyero'><img className = 'github' src='/assets/images/github-mark.png' style={styles.github}></img></a>
+          <a href='https://www.linkedin.com/in/anthony-rodriguez-ortiz'><img className = 'linkedin' src='/assets/images/LinkedIn-Blue-21@2x.png' style={styles.linkedin}></img></a>
+          <a href='https://pubmed.ncbi.nlm.nih.gov/?term=Rodriguez-Ortiz+AE&cauthor_id=35961851'><img className = 'pubmed' src='/assets/images/256px-US-NLM-PubMed-Logo.svg.png' style={styles.pubmed}></img></a>
     </div>
   );
 }
