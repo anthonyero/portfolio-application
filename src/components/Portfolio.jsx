da8678c5d910595dd4e3d79858e6831ab3ead2ae
anthonyero <anthonyero@gmail.com>
2024-05-09 00:27:09 -0500

    Imported PortfolioItem into Portfolio. Made modifications to PortfolioItem and added hover effects. Still require formatting and styling. Defined a portfolio-info JSON object to iterate through

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,16 @@
 import Grid from '@mui/material/Grid';
+import PortfolioItem from './PortfolioItem.jsx';
+import portfolioData from '../assets/portfolio-info.json'; 
+import path from 'path'
+
 
 export default function ResponsiveGrid() {
   return (
     <>
       <Grid container justifyContent='space-evenly' alignItems='stretch'>
-        {Array.from(Array(6)).map((_, index) => (
+        {portfolioData.map((project, index) => (
           <Grid item xs={12} sm={6} md={6} key={index}>
-            <p>Project: {index + 1}</p>
+            <PortfolioItem imagePath={project.imagePath} deployedLink={project.deployedLink} repoLink={project.repoLink} title={project.title}/>
           </Grid>
         ))}
       </Grid>
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,17 +1,36 @@
-import path from 'path';
 import Container from '@mui/material/Container';
+import {useState} from 'react'
 
-export default function PortfolioItem (props) {
-	{imagePath, deployedLink, repoLink, title} = props;
+export default function PortfolioItem ({imagePath, deployedLink, repoLink, title}) {
+	
+	const [isHover, setIsHover] = useState(false);
+
+    const handleMouseEnter = () => {
+       setIsHover(true);
+    };
+
+    const handleMouseLeave = () => {
+       setIsHover(false);
+    };
+
+    const styles = {
+		img: {
+			width: '810px', 
+		 	height: '455px;',
+		 	opacity: isHover ? '40%': '100%'
+		}
+	}
 
 	return (
 		<div>
 			<Container>
-				<img src={imagePath} />
+				<img src={imagePath} style={styles.img}
+				onMouseEnter={handleMouseEnter}
+            	onMouseLeave={handleMouseLeave}/>
 				<Container>
 					<p>{title}</p>
-					<a href={deployedLink} />
-					<a href={repoLink}/>
+					<a href={deployedLink}>Deployed</a>
+					<a href={repoLink}>GitHub</a>
 				</Container>
 			</Container>
 		</div>
