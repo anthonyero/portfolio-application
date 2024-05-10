fa15c8a4bf745379f6954320e6ada6de2760b468
anthonyero <anthonyero@gmail.com>
2024-05-10 18:37:30 -0500

    Defined a container to house technologies I am proficient in and added styling to it

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,31 +2,42 @@
 import resumeFile from '../assets/Rodriguez-Ortiz Anthony - Resume (April 2024).pdf';
 
 export default function Resume() {
+  const styles = {
+    proficiencesContainer: {
+      margin: '20px'
+    }
+  };
+
   return (
     <div>
       <h1>Resume</h1>
-      <a href={resumeFile} download>Download a copy of my resume! </a>
-      <h2>Front-end Proficiencies</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>Responsive design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
+      <p>
+        My 
+        <a href={resumeFile} download>Download a copy of my resume! </a>
+      </p>
+      <section className='proficiencies-container' style={styles.proficiencesContainer}>
+        <h2>Front-end Proficiencies</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
 
-      <h2>Back-end Proficiencies</h2>
-      <ul>
-        <li>APIs</li>
-        <li>Node</li>
-        <li>Express</li>
-        <li>PostgreSQL, Sequelize</li>
-        <li>MongoDB, Mongoose</li>
-        <li>REST</li>
-        <li>GraphQL</li>
-      </ul> 
+        <h2>Back-end Proficiencies</h2>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>PostgreSQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul> 
+      </section>
     </div>
   );
 }
