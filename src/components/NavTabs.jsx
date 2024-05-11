34eb5846854be13e5d495d4a568e5659bc36ba5f
anthonyero <anthonyero@gmail.com>
2024-05-10 20:13:53 -0500

    Added functionality to allow a scrollable interface for the navigation tabs allowing greater flexibility as viewport dimensions change

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -8,7 +8,7 @@ function NavTabs() {
 	const currentPage = useLocation().pathname;
 
 	return (
-		<Tabs value={currentPage}>	
+		<Tabs value={currentPage} variant="scrollable" scrollButtons allowScrollButtonsMobile>	
 			<Tab label='About Me' value='/about' to='/about' component={Link}/>
 			<Tab label='Portfolio' value='/portfolio' to='/portfolio' component={Link}/>
 			<Tab label='Contact' value='/contact' to='/contact' component={Link}/>
