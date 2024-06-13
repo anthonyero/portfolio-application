6619346ab49f89a5d45b991d6e85f1019146ccef
anthonyero <anthonyero@gmail.com>
2024-06-12 21:04:02 -0500

    Added an xs breakpoint to the grid container that will make grid items display vertically on smaller viewports

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ import path from 'path'
 export default function ResponsiveGrid() {
   return (
     <>
-      <Grid container justifyContent='space-evenly' alignItems='stretch'>
+      <Grid container spacing={1} justifyContent='space-evenly' alignItems='stretch' xs={12}>
         {portfolioData.map((project, index) => (
           <Grid item xs={12} sm={6} md={6} key={index}>
             <PortfolioItem imagePath={project.imagePath} deployedLink={project.deployedLink} repoLink={project.repoLink} title={project.title}/>
